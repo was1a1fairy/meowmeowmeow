067b66b7bd55037e4589d1624d4dcf8f3a19163b
Ари <arianna2811@yandex.ru>
2025-11-18 17:30:14 +0300

    task3

--- hw3.py
+++ hw3.py
@@ -17,3 +17,25 @@ async def delayed_print(text, delay):
 
 if __name__ == '__main__':
     asyncio.run(delayed_print('bbb',3))
+
+
+async def cor1():
+    await asyncio.sleep(3)
+    print(1)
+
+async def cor2():
+    await asyncio.sleep(2)
+    print(2)
+
+async def cor3():
+    await asyncio.sleep(1)
+    print(3)
+
+async def main():
+
+    lst_cor = [cor1(),cor2(),cor3()]
+    await asyncio.gather(*lst_cor)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
